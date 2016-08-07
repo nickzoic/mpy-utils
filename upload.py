74675b337dd62416fe2a457c55f1766b55a3bd56
Nick Moore <nick@mnemote.com>
2016-08-07 21:23:18 +1000

    reset and interrupt options

--- upload.py
+++ upload.py
@@ -10,11 +10,15 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--port', default='/dev/ttyUSB0', help='serial port device')
 parser.add_argument('--baud', default=115200, type=int, help='port speed in baud')
 parser.add_argument('--delay', default=100.0, type=float, help='delay between lines (ms)')
-parser.add_argument('files', nargs='+', type=argparse.FileType('rb'))
-
+parser.add_argument('--interrupt', action='store_true', help='send soft interrupt (control-C) before upload')
+parser.add_argument('--reset', action='store_true', help='send soft reset (control-D) after upload')
+parser.add_argument('files', nargs='*', type=argparse.FileType('rb'))
 args = parser.parse_args()
 
 port = serial.Serial(args.port, args.baud)
+
+if args.interrupt:
+  port.write('\x03')
 
 for fh in args.files:
 
@@ -28,5 +32,8 @@ for fh in args.files:
 
   port.write('_fh.close()\r')
   fh.close()
+
+if args.reset:
+  port.write('\x04')
 
 port.close()
