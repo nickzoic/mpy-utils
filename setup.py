e8c61e4e691b6c37be941de64a6cfbd03a83f33f
Nick Moore <nick@zoic.org>
2017-02-21 23:13:50 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='mpy-utils',
-  version='0.1.2',
+  version='0.1.3',
   description='MicroPython development utility programs',
   url='http://github.com/nickzoic/mpy-utils/',
   author='Nick Moore',
