e882325dc1307e17f898859de59823249fd5e9e3
Nick Moore <nick@zoic.org>
2017-07-25 13:30:13 +1000

    remove unnecessary requirement for Python 3

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import setup
 
-import sys
-if sys.version_info < (3,3):
-    sys.exit("Sorry, Python 3.3 or greater is required")
-
 setup(
   name='mpy-utils',
-  version='0.1.6',
+  version='0.1.7',
   description='MicroPython development utility programs',
   url='http://github.com/nickzoic/mpy-utils/',
   author='Nick Moore',
